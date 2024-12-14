f40151b2f6dcd20e959d7bba652f826e8c64402b
sophia <scastellarin@quansight.com>
2024-12-13 16:41:45 -0800

    Load config plugins

--- conda-store-server/conda_store_server/plugins/__init__.py
+++ conda-store-server/conda_store_server/plugins/__init__.py
@@ -3,7 +3,9 @@
 # license that can be found in the LICENSE file.
 
 from conda_store_server._internal.plugins.lock.conda_lock import conda_lock
+from conda_store_server._internal.plugins.config.conda_store import conda_store
 
 BUILTIN_PLUGINS = [
     conda_lock,
+    conda_store,
 ]
