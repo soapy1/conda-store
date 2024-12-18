f8bf29f9c25b4b77377bdc2decb15202299559e0
pdmurray <peynmurray@gmail.com>
2024-12-17 18:31:12 -0800

    Pagination works, but I need a way to get the last values from the results; InstantiatedAttribute can't be used to get values from a db table entry?

--- conda-store-server/conda_store_server/_internal/schema.py
+++ conda-store-server/conda_store_server/_internal/schema.py
@@ -499,10 +499,10 @@ class APIPaginatedResponse(APIResponse):
 
 
 class APICursorPaginatedResponse(BaseModel):
-    data: Optional[Any]
+    data: Optional[Any] = None
     status: APIStatus
-    message: Optional[str]
-    cursor: Optional[str]
+    message: Optional[str] = None
+    cursor: Optional[str] = None
     count: int
 
 
@@ -571,7 +571,7 @@ class APIDeleteNamespaceRole(BaseModel):
 
 # GET /api/v1/environment
 class APIListEnvironment(APICursorPaginatedResponse):
-    data: List[Environment]
+    data: List[Environment] = []
 
 
 # GET /api/v1/environment/{namespace}/{name}
