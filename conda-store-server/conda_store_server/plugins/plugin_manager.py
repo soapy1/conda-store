7b6e07b43e3ef46df28323ae773e7f174449c105
sophia <scastellarin@quansight.com>
2024-12-23 09:36:02 -0800

    Add functions for getting the storage plugin from the plugin manager

--- conda-store-server/conda_store_server/plugins/plugin_manager.py
+++ conda-store-server/conda_store_server/plugins/plugin_manager.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import typing
 import pluggy
 
 from conda_store_server.exception import CondaStorePluginNotFoundError
@@ -17,26 +18,43 @@ class PluginManager(pluggy.PluginManager):
       * discovering and registering CondaStore plugins
     """
 
+    def _get_plugins(self, hook_fn: typing.Callable):
+        """Returns a dict of plugin name to class, given the hook function
+        to run to get the plugin iterator
+        """
+        plugins = [item for items in hook_fn() for item in items]
+        return {p.name.lower(): p for p in plugins}
+
+    def _get_plugin(self, get_plugins_fn: typing.Callable, name: str):
+        """Returns a plugin of a given type and name"""
+        plugins = get_plugins_fn()
+
+        if name not in plugins:
+            raise CondaStorePluginNotFoundError(
+                plugin=name, available_plugins=plugins.keys()
+            )
+
+        return plugins[name]
+
     def get_trait_config_plugins(self) -> dict[str, types.TypeTraitConfigPlugin]:
         """Returns a dict of config plugin name to class"""
-        plugins = [item for items in self.hook.trait_config_plugins() for item in items]
-        return {p.name.lower(): p for p in plugins}
+        return self._get_plugins(self.hook.trait_config_plugins)
 
     def get_lock_plugins(self) -> dict[str, types.TypeLockPlugin]:
         """Returns a dict of lock plugin name to class"""
-        plugins = [item for items in self.hook.lock_plugins() for item in items]
-        return {p.name.lower(): p for p in plugins}
+        return self._get_plugins(self.hook.lock_plugins)
 
     def get_lock_plugin(self, name: str) -> types.TypeLockPlugin:
         """Returns a lock plugin by name"""
-        lockers = self.get_lock_plugins()
+        return self._get_plugin(self.get_lock_plugins, name)
 
-        if name not in lockers:
-            raise CondaStorePluginNotFoundError(
-                plugin=name, available_plugins=lockers.keys()
-            )
+    def get_storage_plugins(self) -> dict[str, types.TypeStoragePlugin]:
+        """Returns a dict of storage plugin name to class"""
+        return self._get_plugins(self.hook.storage_plugins)
 
-        return lockers[name]
+    def get_storage_plugin(self, name: str) -> types.TypeStoragePlugin:
+        """Returns a storage plugin by name"""
+        return self._get_plugin(self.get_storage_plugins, name)
 
     def collect_plugins(self) -> None:
         """Registers all availble plugins"""
